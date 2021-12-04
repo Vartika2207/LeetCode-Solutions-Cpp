b180861d3200a5820e2d192b2bd76574f30609d2
Vartika Shakya <65726781+Vartika2207@users.noreply.github.com>
2021-12-05 04:01:33 +0530

    Added first commit

---
+++ LeetCode Solutions C++/16. 3Sum Closest.cpp
@@ -0,0 +1,33 @@
+class Solution {
+    int threeSum(vector<int>& nums, int target){
+        
+        sort(nums.begin(),nums.end());
+        int closest = nums[0] + nums[1] + nums[2];
+        for(int i=0;i<nums.size()-2;i++){
+                int s=i+1;
+                int e=nums.size()-1;
+                
+                while(s<e){
+                    int sum = nums[i] + nums[e] + nums[s];
+                    
+                    if(abs(-target + sum ) < abs(-target + closest))  {
+                        closest = sum;
+                    } 
+                    
+                    if(target==sum){
+                        break;
+                    }
+                     
+                    else if(target < sum) e--;
+                    else s++;
+            }
+          }
+        return closest;
+    }
+    
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        return threeSum(nums, target);
+        
+    }
+};
