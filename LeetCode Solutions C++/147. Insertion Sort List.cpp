0f85df76596fd5e4995cf58acedac1bb28e6f144
Vartika Shakya <65726781+Vartika2207@users.noreply.github.com>
2022-02-25 01:08:22 +0530

    Added first commit

---
+++ LeetCode Solutions C++/147. Insertion Sort List.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    ListNode* insertionSortList(ListNode* head) {
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+        
+        ListNode *prev = dummy, *curr = head;
+        while(curr){
+            if(curr->next && curr->next->val < curr->val){
+                while(prev->next && prev->next->val < curr->next->val){
+                    prev = prev->next;
+                }
+                ListNode* temp = prev->next;
+                prev->next = curr->next;
+                curr->next = curr->next->next;
+                prev->next->next = temp;
+                prev = dummy;
+            }
+            else{
+                curr = curr->next;
+            }
+        }
+        return dummy->next;
+    }
+};
