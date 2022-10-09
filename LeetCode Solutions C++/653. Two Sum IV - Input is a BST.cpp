1446a198691d663158455c4eaf7a110857e1c06a
Vartika Shakya <65726781+Vartika2207@users.noreply.github.com>
2022-10-09 19:15:12 +0530

    Added iterative and recursive inOrder approach

--- LeetCode Solutions C++/653. Two Sum IV - Input is a BST.cpp
+++ LeetCode Solutions C++/653. Two Sum IV - Input is a BST.cpp
@@ -1,19 +1,41 @@
 //time: O(n+n), space: O(n)
 class Solution {
-  //time:O(n), space: 0(1)
-    void findInorder(TreeNode* root, vector<int>& inOrder){
+    //below is recursive approach
+    void findInorderRec(TreeNode* root, vector<int>& inOrder){
         if(!root)
             return;
         
-        findInOrder(root->left, inOrder);
+        findInorderRec(root->left, inOrder);
         inOrder.push_back(root->val);
-        findInOrder(root->right, inOrder);
+        findInorderRec(root->right, inOrder);
+    }
+    
+    //below is iterative approach
+    void findInorderItr(TreeNode* root, vector<int>& inOrder){
+        if(!root)
+            return;
+        
+        stack<TreeNode*> st;
+        TreeNode* temp = root;
+        while(true){
+            if(temp){
+                st.push(temp);
+                temp = temp->left;
+            }
+            else{
+                if(st.empty()) break;
+                temp = st.top();
+                st.pop();
+                inOrder.push_back(temp->val);
+                temp = temp->right;
+            }
+        }
     }
     
 public:
     bool findTarget(TreeNode* root, int k) {
         vector<int> inOrder;
-        findInorder(root, inOrder);
+        findInorderItr(root, inOrder);
         int start = 0, end = inOrder.size() - 1;
         while(start < end){
             int sum = inOrder[start] + inOrder[end];
