65def1c3ec1208676f6743499c044cd8c615f4d3
Vartika Shakya <65726781+Vartika2207@users.noreply.github.com>
2022-10-16 15:34:53 +0530

    Added optimized approach

--- LeetCode Solutions C++/1335. Minimum Difficulty of a Job Schedule.cpp
+++ LeetCode Solutions C++/1335. Minimum Difficulty of a Job Schedule.cpp
@@ -1,11 +1,40 @@
 class Solution {
-    /************Took hint help************/
-    //time: O(n*n*d) ==> TLE
-    //time Complexity: for *max_ele..   is O(n),  for loop is O(n) and solve is running O(d-1) ~ O(d)
-    int solve(vector<int>& jd, int d, int start){
+    /***********Took hint help***************/
+     //optimized
+    //time: O(nd)
+    int dp[301][11]; //bcz start and d are changing
+    int solve_memo(vector<int>& jd, int d, int start, int n){
+        
+        if(d == 1)
+            return *max_element(jd.begin() + start, jd.end());
+        if(dp[start][d] != -1)
+            return dp[start][d];
+        
+        int minDiffOfJobSchedule = INT_MAX;
+        int diffTaskOfDay = INT_MIN; //bcz it has to be max
+        
+        for(int i=start; i<=n-d; i++){
+            diffTaskOfDay = max(diffTaskOfDay, jd[i]);
+            minDiffOfJobSchedule = min(minDiffOfJobSchedule, diffTaskOfDay + solve_memo(jd, d-1, i+1, n));
+        }
+        dp[start][d] = minDiffOfJobSchedule;
+        return dp[start][d];
+    }
+    
+    int solve_memoHelper(vector<int>& jd, int d){
         int n = jd.size();
-        if(n < d)
-            return -1;
+        memset(dp, -1, sizeof(dp));
+        return solve_memo(jd, d, 0, n);
+        
+    }
+    
+    
+    /****************************/
+    
+    /************Took hint help************/
+    //time: O(not sure) ==> TLE have repeated calls
+    //time Complexity: for *max_ele..   is O(n),  for loop is O(n) and solve is running O(d-1) ~ O(d)
+    int solve(vector<int>& jd, int d, int start, int n){
         
         if(d == 1)
             return *max_element(jd.begin() + start, jd.end());
@@ -15,15 +44,14 @@ class Solution {
         
         for(int i=start; i<=n-d; i++){
             diffTaskOfDay = max(diffTaskOfDay, jd[i]);
-            minDiffOfJobSchedule = min(minDiffOfJobSchedule, diffTaskOfDay + solve(jd, d-1, i+1));
+            minDiffOfJobSchedule = min(minDiffOfJobSchedule, diffTaskOfDay + solve(jd, d-1, i+1, n));
         }
         return minDiffOfJobSchedule;
     }
     
-    
     /****************************/
     
-    /*********My Method, giving WA*********/
+    /*********My Method, WA*********/
     //time: O(d*n), space: O(1)
     int helper(vector<int>& jd, int d, int start){
         int n = jd.size();
@@ -43,6 +71,9 @@ class Solution {
     
 public:
     int minDifficulty(vector<int>& jd, int d) {
-       return solve(jd, d, 0);   
+        int n = jd.size();
+        if(n < d)
+            return -1;
+        return solve_memoHelper(jd, d);   
     }
 };
