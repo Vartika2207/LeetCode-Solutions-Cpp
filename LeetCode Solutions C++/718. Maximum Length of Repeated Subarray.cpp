970a9e9ad68fd771a0c4035f47a021c32cdd90b9
Vartika Shakya <65726781+Vartika2207@users.noreply.github.com>
2022-10-16 19:03:43 +0530

    Added space optimized approach

--- LeetCode Solutions C++/718. Maximum Length of Repeated Subarray.cpp
+++ LeetCode Solutions C++/718. Maximum Length of Repeated Subarray.cpp
@@ -1,7 +1,7 @@
 class Solution {
-public:
-   //time: O(nm), space: O(nm)
-    int findLength(vector<int>& nums1, vector<int>& nums2) {
+    /************DP*************/
+    //time: O(n*m), space: O(n*m)
+    int findLength_dp(vector<int>& nums1, vector<int>& nums2) {
         int n = nums1.size();
         int m = nums2.size();
         
@@ -23,4 +23,36 @@ public:
         }
         return ans;
     }
+    /****************************/
+    
+    /************DP-Space optimization************/
+   //time: O(n*m), space: O(2n)
+    int findLength_dp_optimization(vector<int>& nums1, vector<int>& nums2){
+        int n = nums1.size();
+        int m = nums2.size();
+        
+        if(n < m)
+            nums2.swap(nums1);
+    
+        vector<int> dp(n+1), prev(n+1);
+        
+        int ans = 0;
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=m; j++){
+                if(nums1[i-1] == nums2[j-1])
+                    dp[j] = prev[j-1] + 1;
+                else
+                    dp[j] = 0;
+                
+                ans = max(ans, dp[j]);
+            }
+            prev.swap(dp);
+        }
+        return ans;
+    }
+    /************************/
+public:
+    int findLength(vector<int>& nums1, vector<int>& nums2){
+        return findLength_dp_optimization(nums1, nums2);
+    } 
 };
