46ee32d4e690e579eb288227da1e90af9aa7f298
Vartika Shakya <65726781+Vartika2207@users.noreply.github.com>
2022-10-16 15:28:36 +0530

    Added first commit

---
+++ LeetCode Solutions C++/1335. Minimum Difficulty of a Job Schedule.cpp
@@ -0,0 +1,47 @@
+class Solution {
+    /************Took hint help************/
+    //time: 
+    int solve(vector<int>& jd, int d, int start){
+        int n = jd.size();
+        if(n < d)
+            return -1;
+        
+        if(d == 1)
+            return *max_element(jd.begin() + start, jd.end());
+        
+        int minDiffOfJobSchedule = INT_MAX;
+        int diffTaskOfDay = INT_MIN; //bcz it has to be max
+        
+        for(int i=start; i<=n-d; i++){
+            diffTaskOfDay = max(diffTaskOfDay, jd[i]);
+            minDiffOfJobSchedule = min(minDiffOfJobSchedule, diffTaskOfDay + solve(jd, d-1, i+1));
+        }
+        return minDiffOfJobSchedule;
+    }
+    
+    
+    /****************************/
+    
+    /*********My Method, giving WA*********/
+    //time: O(d*n), space: O(1)
+    int helper(vector<int>& jd, int d, int start){
+        int n = jd.size();
+        if(n < d)
+            return -1;
+        if(d == 1)
+            return *max_element(jd.begin() + start, jd.end());
+        
+        return jd[start] + helper(jd, d-1, start + 1);
+        
+    }
+    int minDifficulty_helper(vector<int>& jd, int d) {
+       sort(jd.begin(), jd.end());
+       return helper(jd, d, 0);   
+    }
+    /*****************************/
+    
+public:
+    int minDifficulty(vector<int>& jd, int d) {
+       return solve(jd, d, 0);   
+    }
+};
