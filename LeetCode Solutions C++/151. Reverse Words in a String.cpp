9a98546230dcafcd4089ba5dc361ee21dd04ae9c
Vartika Shakya <65726781+Vartika2207@users.noreply.github.com>
2022-11-13 22:16:02 +0530

    Added better approach

--- LeetCode Solutions C++/151. Reverse Words in a String.cpp
+++ LeetCode Solutions C++/151. Reverse Words in a String.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-  //space: O(n), time: O(n)
-    string reverseWords(string s) {
+    //time: O(n), space: O(n)
+    string reverseWords_naive(string s) {
         vector<string> rev;
         stringstream ss(s);
         string temp;
@@ -18,5 +18,35 @@ public:
         }
         
         return rev_string;
+    }
+    
+    //time: O(n), space: O(1)
+    string reverseWords_better(string s){
+        reverse(s.begin(), s.end());
+        
+        int l = 0, r = 0, i = 0, n = s.size();
+        while(i < n){
+            while(i < n && s[i] != ' ')
+                s[r++] = s[i++];
+            
+            if(r > l){ //word found in string
+                reverse(s.begin() + l, s.begin() + r);
+                
+                if(r == n) //end of string
+                    break;
+                s[r] = ' '; r++;
+                l = r;
+            }
+            i++; //s[i] =' ', skip this
+        }
+        if(r > 0 and s[r-1] == ' '){ //skip last character
+            r--;
+            s.resize(r);
+        }
+        return s;
+    }
+    
+    string reverseWords(string s){
+        return reverseWords_better(s);
     }
 };
