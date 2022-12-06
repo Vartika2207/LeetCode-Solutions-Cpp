60570494d82cafad77eb5f8cee027ba5c0ef6427
Vartika Shakya <65726781+Vartika2207@users.noreply.github.com>
2022-12-07 00:49:10 +0530

    Updated first commit

--- LeetCode Solutions C++/328. Odd Even Linked List.cpp
+++ LeetCode Solutions C++/328. Odd Even Linked List.cpp
@@ -10,21 +10,25 @@
  */
 class Solution {
 public:
-    ListNode* oddEvenList(ListNode* head) {
-     
-        if(head==NULL || head->next==NULL) return head;
-        ListNode *odd=head;
-        ListNode *even=head->next;
-        ListNode *evenHead=head->next;
+    //time: O(n), space: O(1)
+    ListNode* oddEvenList_naive(ListNode* head) {
+        if(!head or !head->next)
+            return head;
         
-        while(even!=NULL && even->next!=NULL){
-            odd->next=even->next;
-            odd=odd->next;
-            even->next=odd->next;
-            even=even->next;
+        ListNode *odd = head, *even = head->next, *even_temp = even;
+        while(odd and even and even->next){
+            odd->next = even->next;
+            odd = odd->next;
+            if(!odd)
+                break;
+            even->next = odd->next;
+            even = even->next;
         }
-        odd->next=evenHead;
-        return head;
-        
+        odd->next = even_temp;
+        return head;    
+    }
+    
+    ListNode* oddEvenList(ListNode* head){
+        return oddEvenList_naive(head);
     }
 };
