cdc6e0c0706bd9355f391c1ccaa0bbd0d7d4ec4a
Vartika Shakya <65726781+Vartika2207@users.noreply.github.com>
2023-01-11 01:49:49 +0530

    Update 101. Symmetric Tree.cpp

--- LeetCode Solutions C++/101. Symmetric Tree.cpp
+++ LeetCode Solutions C++/101. Symmetric Tree.cpp
@@ -6,7 +6,6 @@ public:
     }
     
     bool isSameTree(TreeNode* root1,TreeNode *root2) {
-    
         if(root1==NULL && root2==NULL) return true;
         if(root1==NULL && root2!=NULL || root1!=NULL && root2==NULL) return false;
         if(root1->val!=root2->val) return false;
