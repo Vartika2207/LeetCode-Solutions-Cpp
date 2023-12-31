853385d55af51b070ea6efc29e734862a23ba741
Vartika Shakya <65726781+Vartika2207@users.noreply.github.com>
2023-12-31 15:24:11 +0530

    Added optimized approach

--- LeetCode Solutions C++/540. Single Element in a Sorted Array.cpp
+++ LeetCode Solutions C++/540. Single Element in a Sorted Array.cpp
@@ -1,15 +1,30 @@
 class Solution {
-public:
-    //time: O(n), space: O(1)
-    int singleNonDuplicate_xor(vector<int>& arr) {
-        int num=arr[0];
-        for(int i=1; i<arr.size(); i++){
-            num ^= arr[i];
+
+//  time: O(n)
+    int singleNonDuplicateXor(vector<int>& nums) {
+        int ans = nums[0];
+        for(auto i = 1; i< nums.size(); i++)
+            ans ^= nums[i];
+        return ans;
+    }
+
+// time: log(n)
+    int singleNonDuplicateBS(vector<int>& nums) {
+        int low = 0, high = nums.size()-1;
+        while(low < high){
+            int mid = (low + high) >> 1;
+            int num = (mid%2==0) ? mid+1 : mid-1; /*split with mid make last even half hence checking if mid 
+            is odd then 1st half is odd and if mid even then 1st half even*/
+            if(nums[mid] == nums[num]) low = mid+1; /*if arr idx at num=mid+1 has same value at mid that means that half even size array has 1 element equal to mid hence 2nd-half-even-len-1 left which is odd since all are paired then that one remaining if in right hence low=mid+1*/
+            else high = mid;
         }
-        return num;
+        return nums[low];
+    }
+
+
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        return singleNonDuplicateBS(nums);
     }
     
-    int singleNonDuplicate(vector<int>& arr) {
-        return singleNonDuplicate_xor(arr);
-    }
 };
