a03cd91fd2f5af8f764d6b298d5e2e67314e217c
Vartika Shakya <65726781+Vartika2207@users.noreply.github.com>
2024-01-21 01:23:07 +0530

    added short approach

--- LeetCode Solutions C++/3010. Divide an Array Into Subarrays With Minimum Cost I.cpp
+++ LeetCode Solutions C++/3010. Divide an Array Into Subarrays With Minimum Cost I.cpp
@@ -12,4 +12,10 @@ public:
         }
         return (first+sec+third);
     }
+
+//short app ------------------------------------------------
+        int minimumCost(vector<int>& nums) {
+                sort(nums.begin() + 1, nums.end());
+                return nums[0] + nums[1] + nums[2];
+            }
 };
