182ad5bb4fe4decef7cb46322cd17a25f6b0aa49
Vartika Shakya <65726781+Vartika2207@users.noreply.github.com>
2024-02-13 01:50:54 +0530

    Added 5 approaches

--- LeetCode Solutions C++/169. Majority Element.cpp
+++ LeetCode Solutions C++/169. Majority Element.cpp
@@ -1,3 +1,68 @@
+
+class Solution {
+    //app0: do binary search to count freq num[i] from j=i,j<n and check where frq>n/2; ->(time: O(n^2), space: O(1))
+   
+    // app1: use map to store freq and find max freq -> (time: O(nlogn+n), space: O(n))
+
+    // app2: sort, use sliding window to find max window length  -> (time: O(nlogn+n), space: O(1))
+    int majorityElement_app2(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        int l = 0, r = 0;
+        int maxLen = 0;
+        int majEle;
+        while(r < n) {
+            if(nums[r] == nums[l]) 
+                r++;
+            else {
+                int windowLen =  r - l;
+                if(windowLen > maxLen) {
+                    maxLen = windowLen;
+                    majEle = nums[l];
+                }
+                l = r;
+            }
+        }
+        int windowLen =  r - l;
+        if(windowLen > maxLen) {
+            maxLen = windowLen;
+            majEle = nums[l];
+        }
+        return majEle;
+    }
+
+    // app3: since majority ele is greater than n/2 hence will surly be mid element -> (time: O(nlogn), space: O(1))
+    int majorityElement_app3(vector<int>& nums) {
+        int n = nums.size();
+        sort(begin(nums), end(nums));
+        return nums[n/2];
+    }
+
+    // app4:boyer moore -> (time: O(n), space: O(1)): https://www.youtube.com/watch?v=nP_ns3uSh80
+    int majorityElement_app4(vector<int>& nums) {
+        int n = nums.size();
+        int count = 0, majEle;
+        int i = 0;
+        while(i < n) {
+            if(count == 0)
+                majEle = nums[i];
+            if(nums[i] == majEle)
+                count++;
+            else 
+                count--;
+            i++;
+        }
+        return majEle;
+    }
+
+
+public:
+    int majorityElement(vector<int>& nums) {
+        return majorityElement_app4(nums);
+    }
+};
+
+///////////////////////////*
 class Solution {
 public:
     
@@ -23,3 +88,4 @@ public:
         return mooreVotingAlgo(nums);
     } 
 };
+*///////////////////////////////
